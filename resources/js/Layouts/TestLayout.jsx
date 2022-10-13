69a7424142f7aba63b20649b21933723b829201e
Irving <adriantp098@gmail.com>
2022-10-13 01:52:57 -0500

    Modificacion Layout principal

--- resources/js/Layouts/TestLayout.jsx
+++ resources/js/Layouts/TestLayout.jsx
@@ -12,30 +12,13 @@ export default function Authenticated({ auth, header, children }) {
     return (
         <div className="flex flex-col min-h-screen bg-gray-100">
             {/* Barra superior */}
-            <nav className="min-w-full   bg-white border-b border-gray-100">
-                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <nav className="min-w-full   bg-indigo-400 border-b border-gray-100 ">
+                <div className="  w-full px-4 sm:px-6 ">
                     <div className="flex justify-between h-16">
-                        <div className="flex">
-                            <div className="shrink-0 flex items-center">
-                                <Link href="/">
-                                    <ApplicationLogo className="block h-9 w-auto text-gray-500" />
-                                </Link>
-                            </div>
-
-                            {/* <div className="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                                <NavLink
-                                    href={route("dashboard")}
-                                    active={route().current("dashboard")}
-                                >
-                                    Dashboard
-                                </NavLink>
-                                <NavLink
-                                    href={route("personal.index")}
-                                    active={route().current("personal.index")}
-                                >
-                                    Personal
-                                </NavLink>
-                            </div> */}
+                        <div className="shrink-0 flex items-center">
+                            <Link href="/">
+                                <ApplicationLogo className="block h-9 w-auto text-gray-500" />
+                            </Link>
                         </div>
 
                         {/* Dropdown Menu */}
@@ -143,6 +126,18 @@ export default function Authenticated({ auth, header, children }) {
                         >
                             Personal
                         </ResponsiveNavLink>
+                        <ResponsiveNavLink
+                            href={route("personal.index")}
+                            active={route().current("personal.index")}
+                        >
+                            Alumnos
+                        </ResponsiveNavLink>
+                        <ResponsiveNavLink
+                            href={route("personal.index")}
+                            active={route().current("personal.index")}
+                        >
+                            Solicitudes
+                        </ResponsiveNavLink>
                     </div>
 
                     <div className="pt-4 pb-1 border-t border-gray-200">
@@ -167,13 +162,11 @@ export default function Authenticated({ auth, header, children }) {
                     </div>
                 </div>
             </nav>
-            
 
             <div className="flex flex-grow overflow-hidden">
-                
                 {/* Menu lateral*/}
-                <div className="hidden sm:flex flex-col items-center w-40 flex-shrink-0 overflow-y-auto text-gray-700 bg-gray-100 rounded">
-                    <a className="flex items-center w-full px-3 mt-3" href="#">
+                <div className="hidden sm:flex flex-col items-center w-45 flex-shrink-0 overflow-y-auto text-gray-700 bg-white ">
+                    {/* <a className="flex items-center w-full px-3 mt-3" href="#">
                         <svg
                             className="w-8 h-8 fill-current"
                             xmlns="http://www.w3.org/2000/svg"
@@ -183,14 +176,13 @@ export default function Authenticated({ auth, header, children }) {
                             <path d="M11 17a1 1 0 001.447.894l4-2A1 1 0 0017 15V9.236a1 1 0 00-1.447-.894l-4 2a1 1 0 00-.553.894V17zM15.211 6.276a1 1 0 000-1.788l-4.764-2.382a1 1 0 00-.894 0L4.789 4.488a1 1 0 000 1.788l4.764 2.382a1 1 0 00.894 0l4.764-2.382zM4.447 8.342A1 1 0 003 9.236V15a1 1 0 00.553.894l4 2A1 1 0 009 17v-5.764a1 1 0 00-.553-.894l-4-2z" />
                         </svg>
                         <span className="ml-2 text-sm font-bold">The App</span>
-                    </a>
+                    </a> */}
                     <div className="w-full px-2">
-                        <div className="flex flex-col items-center w-full mt-3 border-t border-gray-300">
+                        <div className="flex flex-col items-center w-full mt-1 border-gray-300">
                             <NavLink
                                 href={route("dashboard")}
                                 active={route().current("dashboard")}
-                            >   
-
+                            >
                                 <svg
                                     className="w-6 h-6 stroke-current"
                                     xmlns="http://www.w3.org/2000/svg"
@@ -205,16 +197,15 @@ export default function Authenticated({ auth, header, children }) {
                                         d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
                                     />
                                 </svg>
-                                <span className="ml-2 text-sm font-medium">
-                                    Dashboard
+                                <span className="ml-2 text-basefont-medium">
+                                    Inicio
                                 </span>
                             </NavLink>
 
                             <NavLink
-                               href={route('personal.index')} 
-                               active={route().current('personal.index')}
-                            >   
-
+                                href={route("personal.index")}
+                                active={route().current("personal.index")}
+                            >
                                 <svg
                                     className="w-6 h-6 stroke-current"
                                     xmlns="http://www.w3.org/2000/svg"
@@ -229,16 +220,15 @@ export default function Authenticated({ auth, header, children }) {
                                         d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
                                     />
                                 </svg>
-                                <span className="ml-2 text-sm font-medium">
+                                <span className="ml-2 text-base font-medium">
                                     Personal
                                 </span>
                             </NavLink>
 
                             <NavLink
-                               href={route('personal.index')} 
-                               active={route().current('personal.index')}
-                            >   
-
+                                href={route("personal.index")}
+                                active={route().current("personal.index")}
+                            >
                                 <svg
                                     className="w-6 h-6 stroke-current"
                                     xmlns="http://www.w3.org/2000/svg"
@@ -253,16 +243,15 @@ export default function Authenticated({ auth, header, children }) {
                                         d="M16 8v8m-4-5v5m-4-2v2m-2 4h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
                                     />
                                 </svg>
-                                <span className="ml-2 text-sm font-medium">
-                                    Insights
+                                <span className="ml-2 text-base font-medium">
+                                    Alumnos
                                 </span>
                             </NavLink>
 
                             <NavLink
-                               href={route('personal.index')} 
-                               active={route().current('personal.index')}
-                            >   
-
+                                href={route("personal.index")}
+                                active={route().current("personal.index")}
+                            >
                                 <svg
                                     className="w-6 h-6 stroke-current"
                                     xmlns="http://www.w3.org/2000/svg"
@@ -278,7 +267,7 @@ export default function Authenticated({ auth, header, children }) {
                                     />
                                 </svg>
                                 <span className="ml-2 text-base font-medium">
-                                    Documentos 
+                                    Solicitudes
                                 </span>
                             </NavLink>
                         </div>
@@ -301,7 +290,7 @@ export default function Authenticated({ auth, header, children }) {
                                         d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
                                     />
                                 </svg>
-                                <span className="ml-2 text-sm font-medium">
+                                <span className="ml-2 text-base font-medium">
                                     Products
                                 </span>
                             </a>
@@ -323,7 +312,7 @@ export default function Authenticated({ auth, header, children }) {
                                         d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
                                     />
                                 </svg>
-                                <span className="ml-2 text-sm font-medium">
+                                <span className="ml-2 text-base font-medium">
                                     Settings
                                 </span>
                             </a>
@@ -345,22 +334,24 @@ export default function Authenticated({ auth, header, children }) {
                                         d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
                                     />
                                 </svg>
-                                <span className="ml-2 text-sm font-medium">
+                                <span className="ml-2 text-base font-medium">
                                     Messages
                                 </span>
                                 <span className="absolute top-0 left-0 w-2 h-2 mt-2 ml-2 bg-indigo-500 rounded-full"></span>
                             </a>
                         </div>
                     </div>
-                  
                 </div>
 
-                
-
-                <main scroll-region className='flex-1 px-4 py-8 md:p-12 md:overflow-y-auto bg-white'>
+                <main
+                    scroll-region
+                    className="flex-1 px-4 py-8 md:p-12 md:overflow-y-auto bg-gray-50"
+                >
                     {header && (
                         <header>
-                            <div className="mb-8 text-3xl font-bold">{header}</div>
+                            <div className="mb-8 text-3xl font-bold">
+                                {header}
+                            </div>
                         </header>
                     )}
                     {children}
